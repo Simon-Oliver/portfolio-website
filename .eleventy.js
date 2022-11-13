4cf1f58c82375146bac673858c3e44f2844429a7
Simon-Oliver <stauffer.simon@outlook.com>
2022-11-13 18:47:29 +1100

    Added some basic styling

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("src/css");
   return {
+    passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
     templateFormats: ["html", "njk", "md"],
     dir: {
